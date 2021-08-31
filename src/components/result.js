c3603e72e79979759c1ce8063d00bdbbd82a2ad1
Cjorozco <jose@draxo.co>
2021-08-30 23:43:06 -0500

    List

--- src/components/result.js
+++ src/components/result.js
@@ -8,9 +8,6 @@ const Title = styled.h1`
   align-content: center;
   justify-content: center;
   margin: 40px 0;
-  p span {
-    display: block;
-  }
 `;
 
 const ResultScreen = ({score, setScore, results, getData}) => {
@@ -27,6 +24,16 @@ const ResultScreen = ({score, setScore, results, getData}) => {
       <Title>
         You Scored is {score} / {results.length}
       </Title>
+      {
+        results.map((re, i) =>
+          <ul className="list-group" key={i}>
+            <li className="list-group-item d-flex flex-wrap align-items-center justify-content-center">
+              <span className="mb-2">{re.question}</span>
+              <span className="mb-2">The answer is <b>{re.correct_answer}</b></span>
+            </li>
+          </ul>
+        )
+      }
       <div className="d-flex flex-column  align-items-center justify-content-center mt-5">
         <button type="button" className="btn btn-primary" onClick={playAgain}>
           Play Again
